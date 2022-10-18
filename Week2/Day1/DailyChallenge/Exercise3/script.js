e6934ccbfc839a9930ca4b984e7a843a7e09d741
daniel <raitandaniel@gmail.com>
2022-10-18 20:58:36 +0300

    final project

--- Week2/Day1/DailyChallenge/Exercise3/script.js
+++
@@ -1,7 +0,0 @@
-const addressNumber = 2 ;
-const addressStreet = "BenYehuda" ;
-const country = "israel" ;
-
-const sentence = `I live in ${country} ${addressStreet} ${addressNumber}` ;
-
-console.log(`global address`, sentence)
