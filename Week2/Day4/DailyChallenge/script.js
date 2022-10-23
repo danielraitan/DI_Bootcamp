873c7104fee4f70c0db113f2967931468df05e78
Amichai Berger <76897731+Amichaiz@users.noreply.github.com>
2022-10-23 16:07:46 +0300

    Update script.js

--- Week2/Day4/DailyChallenge/script.js
+++ Week2/Day4/DailyChallenge/script.js
@@ -40,5 +40,40 @@ function createDelimiterRow() {
     return row;
 }
 
+////////////////////*****************************CHECKER
 
+SAMPLE CODE TRY THIS
+
+const promptInput = prompt("Enter comma separated words");
+const words = promptInput.split(",");
+function findLongestWord() {
+    //find the longest word
+    let max = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > max) {
+            max = words[i].length;
+        }
+    }
+    return max;
+}
+function printWords(words,max) {
+    firstAndLast(max);
+    for (let word of words) {
+        console.log(`* ${word + ' '.repeat(max - word.length)} *`);
+    }
+    firstAndLast(max);
+
+}
+function firstAndLast(max) {
+    console.log('*'.repeat(max + 4));
+}
+printWords(words,findLongestWord());
+
+
+
+
+
+
+********************////////////////////////
+ WHEN i Ran your code I got errors
 
