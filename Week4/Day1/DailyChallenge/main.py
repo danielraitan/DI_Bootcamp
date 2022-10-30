0c73d83874a6d3cf06b8c78b993d13e4132b6bc3
daniel <raitandaniel@gmail.com>
2022-10-30 14:59:12 +0200

    DailyChallenge

---
+++ Week4/Day1/DailyChallenge/main.py
@@ -0,0 +1,52 @@
+# 1#
+# for i in range(4): print("hello world")
+
+# 2#
+# question = (99^3) * 8 
+# print(question)
+
+# 3#
+# 5 < 3 = false
+# 3 == 3 = true
+# 3 == "3" = false
+# "3" > 3 = false/error
+# "Hello" == "hello" = false
+
+# 4#
+# computer_brand = "lenovo"
+# print('I have a ' + computer_brand + ' computer')
+
+# 5#
+# name = "Daniel"
+# age = "21"
+# shoe_size = "43"
+# info = "Hey my name is " + name + " im " + age + " and my shoe size is " + shoe_size
+# print(info)
+
+# 6#
+# a = 4
+# b = 2
+# if a > b:
+#     print("Hello World")
+
+# 7#
+# ask = int(input("pick a number:"))
+# if ask % 2 == 0:
+#   print("number is even")
+# elif ask % 2 == 1 :
+#     print("number is odd")  
+
+# 8#
+# name = "Daniel"
+# question = input("what is your name:")
+# if question == "Daniel":
+#   print("nice name just like mine")
+# else :
+#     print("change your name enter new name down below ;)")  
+
+# 9#
+# height = input("what's you height?")
+# if height > "1.45":
+#   print("they are tall enough to ride")
+# else :
+#     print("they need to grow some more to ride") 
