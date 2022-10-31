019180cad9907a4ae5074890bef291362a2b03da
daniel <raitandaniel@gmail.com>
2022-10-31 09:59:59 +0200

    edits

--- Week4/Day1/ExerciseXP/main.py
+++ Week4/Day1/ExerciseXP/main.py
@@ -1,18 +1,52 @@
-question = input("enter a word: ")
+# 1#
+# for i in range(4): print("hello world")
 
-answer = question
-x = int(len(question))
+# 2#
+# question = (99^3) * 8 
+# print(question)
 
-if x < 10 :
-    print("string not long enough")
-elif x > 10 :
-    print("string too long")
+# 3#
+# 5 < 3 = false
+# 3 == 3 = true
+# 3 == "3" = false
+# "3" > 3 = false/error
+# "Hello" == "hello" = false
 
-print(answer[0])
-print(answer[-1])
+# 4#
+# computer_brand = "lenovo"
+# print('I have a ' + computer_brand + ' computer')
 
-mystr = answer
-for i in range(len(mystr)+1):
-    print (mystr[:i])
+# 5#
+# name = "Daniel"
+# age = "21"
+# shoe_size = "43"
+# info = "Hey my name is " + name + " im " + age + " and my shoe size is " + shoe_size
+# print(info)
 
+# 6#
+# a = 4
+# b = 2
+# if a > b:
+#     print("Hello World")
 
+# 7#
+# ask = int(input("pick a number:"))
+# if ask % 2 == 0:
+#   print("number is even")
+# elif ask % 2 == 1 :
+#     print("number is odd")  
+
+# 8#
+# name = "Daniel"
+# question = input("what is your name:")
+# if question == "Daniel":
+#   print("nice name just like mine")
+# else :
+#     print("change your name enter new name down below ;)")  
+
+# 9#
+# height = input("what's you height?")
+# if height > "1.45":
+#   print("they are tall enough to ride")
+# else :
+#     print("they need to grow some more to ride") 
