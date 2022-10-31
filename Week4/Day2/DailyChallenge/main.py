0ec7cc7ded6aecbfb237e7a44b67c4f01885c0d3
daniel <raitandaniel@gmail.com>
2022-10-31 19:00:58 +0200

    DailyChallenge

---
+++ Week4/Day2/DailyChallenge/main.py
@@ -0,0 +1,11 @@
+# challenge 1#
+# first_value = int(input("pick value:"))
+# first_length = int(input("pick length:"))
+# def multiples(value, length):
+#     return [value * i for i in range(1, length + 1)]
+# print(multiples(first_value, first_length))
+
+
+# challenge 2#
+# user_word = set(input("enter:"))
+# print(user_word).replace(',', '')
---
+++ Week4/Day2/ExerciseXP/main.py
@@ -0,0 +1,57 @@
+# 1#
+# my_fav_numbers = {1,2,3}
+# new_num = [4, 5]
+# my_fav_numbers.update(new_num)
+# my_fav_numbers.remove (5)
+# print(my_fav_numbers)
+# friend_fav_numbers = {10, 20, 30} 
+# our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+# print(our_fav_numbers)
+# 2#
+# no
+# 3#
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# basket.remove('Banana')
+# basket.remove('Blueberries')
+# basket.append('Kiwi')
+# basket.insert(0, "Apples")
+# basket.clear()
+# print(basket)
+# 4#
+# float will show you the exact number incluing(1.0)
+# list = [1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+# print(list)
+# 5#
+# for i in range(21):
+#     print('num:', i)
+# for i in range(0,21,2):
+#     print('num:', i)
+# 6#
+# name = ""
+# while name != 'Quit':
+#     name = input("Your input (Quit for exit): ")
+#     if name == 'daniel':
+#         break
+# 7#
+# friuts = input("what friuts do you like?")
+# friut_list=[friuts]
+# print(friut_list)
+# question = input("pick a friut:")
+# for item in friut_list:
+#     if item == question:
+#         print("You chose one of your favorite fruits! Enjoy!")
+#     else:
+#         print("You chose a new fruit. I hope you enjoy")
+# 8#
+# pizza_toppings = ""
+# while pizza_toppings != 'go':
+#     pizza_toppings = input("enter toppings(Quit for exit):")
+#     print( pizza_toppings)
+#     if pizza_toppings == 'Quit':
+#         print(pizza_toppings)
+#         break
+# 9#
+
+
+   
+
