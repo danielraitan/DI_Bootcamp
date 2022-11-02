5525e66528f26a9979082e5a484ea345cabd8d1c
daniel <raitandaniel@gmail.com>
2022-11-02 19:30:09 +0200

    DailyChallenge

---
+++ Week4/Day3/DailyChallenge/main.py
@@ -0,0 +1,21 @@
+# 1#
+# ask = input("enter a word:")
+# print(set(ask))
+# 2#
+# wallet = {
+#     "money": 50
+#     }
+# items_purchase = {
+#   "Water": 1,
+#   "Bread": 3,
+#   "Apple": 4,
+#   "Spoon": 2,
+#   "Fertilizer": 20
+# }
+# r = sorted(items_purchase.keys())
+# print(r)
+
+# if items_purchase.values() < wallet.values():
+#          print(wallet.values())
+# else:
+#         print("nothing")
