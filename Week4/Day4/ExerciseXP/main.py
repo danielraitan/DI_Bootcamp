356693b6d291bb4ef1ae46912f3759aa8fa11c02
daniel <raitandaniel@gmail.com>
2022-11-03 18:13:44 +0200

    ExerciseXP

---
+++ Week4/Day4/ExerciseXP/main.py
@@ -0,0 +1,76 @@
+# 1#
+# def display_message():
+#     print("hey everyone im learning python!")
+# display_message()
+
+# 2#
+# def favorite_book(title):
+#     print(f'One of my favorite books is {title}')
+# favorite_book("Alice in Wonderland")
+
+# 3#
+# def describe_city(city, country):
+#     print(f"{city} is in {country}")
+# describe_city("Madrid", "Spain")
+
+# 4#
+# def accept_a_number(number):
+#     print("your number:", number)
+#     import random
+#     random_num = random.randint(0,10)
+#     print("random number", random_num)
+#     if number ==  random_num:
+#         print("you chose the same number!")
+#     else:
+#         print(f"you chose {number} and not {random_num}")
+# accept_a_number(4)
+
+# 5#
+# def make_shirt(size, text):
+#     size = "large"
+#     text = "I love Python"
+#     print(f"The size of the shirt is {size} and the text is {text}")
+# make_shirt("","")
+# make_shirt("med", "")
+# make_shirt("small", "hello, world")
+
+# 6#
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+# def show_magicians():
+#     print(magician_names)
+# show_magicians()
+# def make_great():
+#     for idx in range(len(magician_names)):
+#      magician_names[idx] = magician_names[idx] + " the great"
+#     print(magician_names)
+# make_great()
+# show_magicians()
+
+# 7#
+# def get_random_temp():
+#     import random
+#     dog = random.randint(-10,40)
+#     return dog
+# get_random_temp()
+
+# def main():
+#     inform = get_random_temp()
+#     print(inform)
+#     print(f"the temp is {inform} degrees celsius")
+#     if inform < 0:
+#        print("Brrr, that's freezing! Wear some extra layers today") 
+#     elif inform > 0 & inform < 16:
+#         print("Quite chilly! Don't forget your coat")
+#     elif inform > 16 & inform < 23:
+#         print("good weather")
+#     elif inform > 24 & inform < 32:
+#         print("little hot outside")
+#     elif inform > 32 & inform < 40:
+#         print("very hot out side")
+# main()
+
+
+
+
+
+    
