ae35627b8b41d891e7eced993dbab31e17ab55f7
daniel <raitandaniel@gmail.com>
2022-11-10 15:33:57 +0200

    Daily Challenge

---
+++ Week5/Day5/DailyChallenge/main.py
@@ -0,0 +1,38 @@
+# 1
+# What is a class?
+# class class_name:
+# What is an instance?
+# def __init__
+# What is encapsulation?
+# its to wrap up both data and methods into one single unit
+# What is abstraction?
+# hides unnecessary information
+# What is inheritance?
+# inherits from another class
+# What is multiple inheritance?
+# inherits from more than one class
+# What is polymorphism?
+# defines methods that have the same name as the methods in the parent class
+# What is method resolution order or MRO?
+# It is the order in which a method is searched for in a classes hierarchy
+
+# 2
+# import random
+# class Card:
+
+#     def __init__(self, deck):
+#         self.points = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+#         self.sign = ['heart','club','diamond','spade']
+#         self.deck = deck
+      
+#     def pick_card(self):
+#         random_point = random.choice(self.points)
+#         random_sign = random.choice(self.sign)
+#         random_card = random_point,random_sign
+#         print(random_card)
+
+# deck = Card("Random card")
+# deck.pick_card()
+
+
+    
