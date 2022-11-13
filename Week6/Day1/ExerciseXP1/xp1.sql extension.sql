1062348b8c1d21c5e93228849e7e6baac05a1a78
daniel <raitandaniel@gmail.com>
2022-11-13 17:51:34 +0200

    XP1

---
+++ Week6/Day1/ExerciseXP1/xp1.sql extension.sql
@@ -0,0 +1,6 @@
+select * from items;
+select * from items where price > 80;
+select * from items where price <= 300;
+select * from customers WHERE last_name = 'Smith'
+select * from customers WHERE last_name = 'Jones'
+select * from customers WHERE last_name != 'Scott'
