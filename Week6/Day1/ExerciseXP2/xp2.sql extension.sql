63ee6c7cdd57b88793579812c4550de646e7cdd6
daniel <raitandaniel@gmail.com>
2022-11-13 17:53:40 +0200

    Xp2

---
+++ Week6/Day1/ExerciseXP2/xp2.sql extension.sql
@@ -0,0 +1,10 @@
+-- select * from students;
+-- select first_name, last_name FROM students;
+-- select first_name, last_name FROM students WHERE id = 2;
+-- select first_name, last_name FROM students WHERE first_name = 'Marc'and last_name = 'Benichou';
+-- select first_name, last_name FROM students WHERE first_name = 'Marc'or last_name = 'Benichou';
+-- select first_name, last_name FROM students where first_name Like '%a%';
+-- select first_name, last_name FROM students where first_name Like 'a%';
+-- select first_name, last_name FROM students where first_name Like '%a';
+-- select first_name, last_name FROM students where id = 1 and id = 3;
+-- select * from students WHERE birth_date >= '2000-01-01';
